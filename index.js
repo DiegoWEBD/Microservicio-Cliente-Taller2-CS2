0e2f583d5dc79c6129771fd882c39449cc426aeb
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-11-14 09:22:05 -0300

    nuevo endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import axios from 'axios'
 
 dotenv.config()
 const PORT = process.env.PORT || 3000
@@ -13,6 +14,26 @@ app.get('/', (_, res) => {
 	res.status(200).json({
 		message: 'Api del Microservicio del cliente.',
 	})
+})
+
+app.get('/devices', (_, res) => {
+	const PERSONAL_ACCESS_TOKEN = process.env.PERSONAL_ACCESS_TOKEN
+	const headers = {
+		Authorization: `Bearer ${PERSONAL_ACCESS_TOKEN}`,
+	}
+
+	axios
+		.post(
+			'https://hosted.mender.io/api/management/v1/useradm/auth/login',
+			{},
+			{ headers }
+		)
+		.then((response) => res.status(200).json({ response }))
+		.catch((error) =>
+			res.status(400).json({
+				error,
+			})
+		)
 })
 
 app.listen(PORT, () => {
